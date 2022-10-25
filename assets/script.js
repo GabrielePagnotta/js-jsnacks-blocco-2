c98be4983982c00158bf4083b7c58ee8c0db8f66
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2022-10-25 17:33:36 +0200

    first commit

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+// snack 1
+
+// let numero = parseInt(prompt("inserisci numero"))
+// console.log(numero)
+// if(numero % 2 == 0){
+//     console.log(numero)
+// }
+
+// else{
+//     let successivo = numero + 1;
+//     console.log(successivo)
+// }
+
+// --------------------------------------------------------------------------
+
+// snack 2
+
+// let nomi = ['peppino', 'luigi', 'sandro'];
+// let cognomi = ['sfriso','martinez','vannucci']
+// let listavuota = []
+
+    
+
+// for (let i = 0; i < 100; i++) {
+//     let numero = Math.floor(Math.random() * 3) + 0;
+//     let numero2 = Math.floor(Math.random() * 3) + 0;
+//     let nome = nomi[numero]
+//     let cognome = cognomi[numero2]
+//     let nome_completo = `${nome} ${cognome}`
+//     console.log(nome_completo)
+    
+//     listavuota.push(nome_completo)
+// }
+
+// console.log(listavuota)
+
+// snack 3
+
+let num = ["1","2","3","4","5","6","7","8"]
+
+
+
+
+
