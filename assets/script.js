0df3cb906c22e17f9214b1d7f18ca21a585ae4a8
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2022-10-25 22:58:41 +0200

    push finale 2

--- assets/script.js
+++ assets/script.js
@@ -49,6 +49,49 @@
 // }
 // console.log(somma)
 
+// *Snack4 (Bonus)*
+
+// Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+// creo array 1
+
+let array_1 = [0,1,2];
+let lunghezza_1 = array_1.length;
+
+// creo array 2
+
+let array_2 =[0,1,2,3];
+let lunghezza_2 = array_2.length;
+
+if (lunghezza_1 > lunghezza_2) {
+    for (let i = lunghezza_2; i < lunghezza_1; i++) {
+        array_2.push(lunghezza_2)
+        lunghezza_2++
+    }
+    
+}else if(lunghezza_1 < lunghezza_2){
+    for (let i = lunghezza_1; i < lunghezza_2; i++) {
+        array_2.push(lunghezza_1)
+        lunghezza_1++
+    }
+    
+}
+
+console.log(lunghezza_1, lunghezza_2)
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
 
 
 
